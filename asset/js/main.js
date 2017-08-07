38b9ad112d60b6b002981f8d0cd37906ff08245b
haidy <haidy>
2017-08-06 20:10:05 -0500

    index

--- asset/js/main.js
+++ asset/js/main.js
@@ -41,7 +41,7 @@ for (var i = 0; i < mapa.length; i++) {
 	
 				celda.style.backgroundColor = "green"
 				var imagen = document.createElement("img"); //etiqueta de imagen
-				imagen.src = "img/arriba.png";
+				imagen.src = "asset/img/arriba.png";
 				celda.appendChild(imagen);
 
 				celdaActual ={x:i,y:j,direccion:"arriba"}; // estamos dandole atributos al objeto celdaActual.
@@ -61,28 +61,28 @@ laberinto.appendChild(tabla);
 der.onclick = function(){
 	if(celdaActual.direccion == "arriba"){
 		var imagen = document.createElement("img");
-			imagen.src = "img/derecha.png";
+			imagen.src = "asset/img/derecha.png";
 			celdaActual.direccion = "derecha";
 			celdas[celdaActual.x][celdaActual.y].removeChild(celdas[celdaActual.x][celdaActual.y].firstChild);
 			celdas[celdaActual.x][celdaActual.y].appendChild(imagen);
 	}
 	else if(celdaActual.direccion == "derecha"){
 		var imagen = document.createElement("img");
-			imagen.src = "img/abajo.png";
+			imagen.src = "asset/img/abajo.png";
 			celdaActual.direccion = "abajo";
 			celdas[celdaActual.x][celdaActual.y].removeChild(celdas[celdaActual.x][celdaActual.y].firstChild);
 			celdas[celdaActual.x][celdaActual.y].appendChild(imagen);
 	}
 	else if(celdaActual.direccion == "abajo"){
 		var imagen = document.createElement("img");
-			imagen.src = "img/izquierda.png";
+			imagen.src = "asset/img/izquierda.png";
 			celdaActual.direccion = "izquierda";
 			celdas[celdaActual.x][celdaActual.y].removeChild(celdas[celdaActual.x][celdaActual.y].firstChild);
 			celdas[celdaActual.x][celdaActual.y].appendChild(imagen);
 	}
 	else if(celdaActual.direccion == "izquierda"){
 		var imagen = document.createElement("img");
-			imagen.src = "img/arriba.png";
+			imagen.src = "asset/img/arriba.png";
 			celdaActual.direccion = "arriba";
 			celdas[celdaActual.x][celdaActual.y].removeChild(celdas[celdaActual.x][celdaActual.y].firstChild);
 			celdas[celdaActual.x][celdaActual.y].appendChild(imagen);
@@ -92,28 +92,28 @@ der.onclick = function(){
 izq.onclick = function(){
 	if(celdaActual.direccion == "arriba"){
 		var imagen = document.createElement("img");
-			imagen.src = "img/izquierda.png";
+			imagen.src = "asset/img/izquierda.png";
 			celdaActual.direccion = "izquierda";
 			celdas[celdaActual.x][celdaActual.y].removeChild(celdas[celdaActual.x][celdaActual.y].firstChild);
 			celdas[celdaActual.x][celdaActual.y].appendChild(imagen);
 	}
 	else if(celdaActual.direccion == "derecha"){
 		var imagen = document.createElement("img");
-			imagen.src = "img/arriba.png";
+			imagen.src = "asset/img/arriba.png";
 			celdaActual.direccion = "arriba";
 			celdas[celdaActual.x][celdaActual.y].removeChild(celdas[celdaActual.x][celdaActual.y].firstChild);
 			celdas[celdaActual.x][celdaActual.y].appendChild(imagen);
 	}
 	else if(celdaActual.direccion == "abajo"){
 		var imagen = document.createElement("img");
-			imagen.src = "img/derecha.png";
+			imagen.src = "asset/img/derecha.png";
 			celdaActual.direccion = "derecha";
 			celdas[celdaActual.x][celdaActual.y].removeChild(celdas[celdaActual.x][celdaActual.y].firstChild);
 			celdas[celdaActual.x][celdaActual.y].appendChild(imagen);
 	}
 	else if(celdaActual.direccion == "izquierda"){
 		var imagen = document.createElement("img");
-			imagen.src = "img/abajo.png";
+			imagen.src = "asset/img/abajo.png";
 			celdaActual.direccion = "abajo";
 			celdas[celdaActual.x][celdaActual.y].removeChild(celdas[celdaActual.x][celdaActual.y].firstChild);
 			celdas[celdaActual.x][celdaActual.y].appendChild(imagen);
@@ -125,7 +125,7 @@ avanzar.onclick = function(){
 	if(celdaActual.direccion== "arriba"){ // Para evaluar a donde esta apuntando la flecha
 		if(mapa[celdaActual.x-1][celdaActual.y] == "_" ){ //Para evaluar si no hay muro 
 			var imagen = document.createElement("img");
-			imagen.src = "img/arriba.png";
+			imagen.src = "asset/img/arriba.png";
 			celdas[celdaActual.x][celdaActual.y].removeChild(celdas[celdaActual.x][celdaActual.y].firstChild);
 			celdaActual.x = celdaActual.x-1;
 			celdas[celdaActual.x][celdaActual.y].appendChild(imagen);
@@ -134,7 +134,7 @@ avanzar.onclick = function(){
 	else if(celdaActual.direccion == "derecha"){
 		if(mapa[celdaActual.x][celdaActual.y +1] == "_" ){ 
 			var imagen = document.createElement("img"); //etiqueta de imagen
-			imagen.src = "img/derecha.png";
+			imagen.src = "asset/img/derecha.png";
 			celdas[celdaActual.x][celdaActual.y].removeChild(celdas[celdaActual.x][celdaActual.y].firstChild);
 			celdaActual.y = celdaActual.y + 1;
 			celdas[celdaActual.x][celdaActual.y].appendChild(imagen);
@@ -143,7 +143,7 @@ avanzar.onclick = function(){
 	else if(celdaActual.direccion== "izquierda"){
 		if(mapa[celdaActual.x][celdaActual.y - 1] == "_" ){ 
 			var imagen = document.createElement("img"); //etiqueta de imagen
-			imagen.src = "img/izquierda.png";
+			imagen.src = "asset/img/izquierda.png";
 			celdas[celdaActual.x][celdaActual.y].removeChild(celdas[celdaActual.x][celdaActual.y].firstChild);
 			celdaActual.y = celdaActual.y - 1;
 			celdas[celdaActual.x][celdaActual.y].appendChild(imagen);
@@ -152,7 +152,7 @@ avanzar.onclick = function(){
 	else if(celdaActual.direccion=="abajo"){
 		if(mapa[celdaActual.x + 1][celdaActual.y] == "_" ){ 
 			var imagen = document.createElement("img"); //etiqueta de imagen
-			imagen.src = "img/abajo.png";
+			imagen.src = "asset/img/abajo.png";
 			celdas[celdaActual.x][celdaActual.y].removeChild(celdas[celdaActual.x][celdaActual.y].firstChild);
 			celdaActual.x = celdaActual.x+1;
 			celdas[celdaActual.x][celdaActual.y].appendChild(imagen);
